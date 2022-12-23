a0f1e7cac833b723579a7192fb0b5cf463a8113b
Seongbae94 <seongbaelee944@gmail.com>
2022-12-23 23:03:21 +0900

    feat: 주문내역 추가

--- src/App.js
+++ src/App.js
@@ -3,12 +3,33 @@ import { Routes, Route } from "react-router-dom";
 import Test from "./components/Test";
 import BestPage from "./pages/BestPage";
 import CharacterPage from "./pages/CharacterPage";
+import OrderHistory from "./pages/OrderHistory";
+import MyPageLayout from "./components/layout/MyPageLayout";
+import Orderbasket from "./pages/Orderbasket";
 function App() {
   return (
     <Routes>
       <Route path="/" element={<BestPage />}></Route>
       <Route path="/" element={<Test />}></Route>
       <Route path="/guan" element={<CharacterPage />}></Route>
+      <Route
+        path="/mypage/orderlist"
+        element={
+          <div>
+            <MyPageLayout />
+            <OrderHistory />
+          </div>
+        }
+      ></Route>
+      <Route
+        path="/mypage/basket"
+        element={
+          <div>
+            <MyPageLayout />
+            <Orderbasket />
+          </div>
+        }
+      ></Route>
     </Routes>
   );
 }
