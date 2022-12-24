77f12e37b97966a3d644b85156e846773f2969c9
Seongbae94 <seongbaelee944@gmail.com>
2022-12-25 00:49:09 +0900

    feat: 장바구니 페이지 업데이트
    상품 선택에 따라 가격 변동 기능 추가

---
+++ src/redux/modules/basketSlice.js
@@ -0,0 +1,149 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  dummyInfo: [
+    {
+      id: 1,
+      title: "러기지택(캐리어)_라이언",
+      price: 13000,
+      num: 1,
+      toggle: false,
+    },
+    {
+      id: 2,
+      title: "러기지택(캐리어)_라이언킹",
+      price: 16000,
+      num: 2,
+      toggle: false,
+    },
+    {
+      id: 3,
+      title: "러기지택(캐리어)_라이언킹킹",
+      price: 20000,
+      num: 1,
+      toggle: false,
+    },
+  ],
+  totalPrice: 0,
+  singlePrice: 0,
+  toggle: false,
+  isLoading: false, // 서버에서 todos를 가져오는 상태를 나타내는 값
+  error: null, // 서버와의 통신이 실패한 경우
+};
+
+export const basketSlice = createSlice({
+  name: "dummyInfo",
+  initialState,
+  reducers: {
+    addProductCount: (state, action) => {
+      state.dummyInfo = state.dummyInfo.map((dummy) =>
+        action.payload === dummy.id
+          ? { ...dummy, num: dummy.num + 1 }
+          : { ...dummy }
+      );
+    },
+
+    subProductCount: (state, action) => {
+      state.dummyInfo = state.dummyInfo.map((dummy) =>
+        action.payload === dummy.id
+          ? { ...dummy, num: dummy.num - 1 }
+          : { ...dummy }
+      );
+    },
+
+    addAllSum: (state) => {
+      const totalPrices = state.dummyInfo.map(
+        (dummy) => state.totalPrice + dummy.num * dummy.price
+      );
+
+      return {
+        ...state,
+        totalPrice: totalPrices.reduce((a, b) => a + b),
+      };
+    },
+
+    subAllSum: (state) => {
+      state.totalPrice = 0;
+    },
+
+    addPriceByCount: (state, action) => {
+      const filtered = state.dummyInfo.filter(
+        (dummy) => dummy.id === action.payload
+      );
+      return {
+        ...state,
+        totalPrice: state.totalPrice + filtered[0].price,
+      };
+    },
+
+    addEachCheckedTotal: (state, action) => {
+      const filtered = state.dummyInfo.filter(
+        (dummy) => dummy.id === action.payload
+      );
+      return {
+        ...state,
+        totalPrice: state.totalPrice + filtered[0].price * filtered[0].num,
+      };
+    },
+    subPriceByCount: (state, action) => {
+      const filtered = state.dummyInfo.filter(
+        (dummy) => dummy.id === action.payload
+      );
+      return {
+        ...state,
+        totalPrice: state.totalPrice - filtered[0].price,
+      };
+    },
+    subEachCheckedTotal: (state, action) => {
+      const filtered = state.dummyInfo.filter(
+        (dummy) => dummy.id === action.payload
+      );
+      return {
+        ...state,
+        totalPrice: state.totalPrice - filtered[0].price * filtered[0].num,
+      };
+    },
+
+    toggleCheck: (state) => (state = { ...state, toggle: !state.toggle }),
+
+    initUpdateStatus: (state) => {
+      state.updateSuccess = false;
+      state.error = null;
+    },
+    initDeleteStatus: (state) => {
+      state.deleteSuccess = false;
+      state.error = null;
+    },
+    clearError: (state) => {
+      state.error = null;
+    },
+  },
+  extraReducers: {
+    // [__getComments.pending]: (state, action) => {
+    //   state.isLoading = true;
+    // },
+    // [__getComments.fulfilled]: (state, action) => {
+    //   state.isLoading = false;
+    //   state.comments = action.payload;
+    // },
+    // [__getComments.rejected]: (state, action) => {
+    //   state.isLoading = false;
+    //   state.error = action.payload;
+    // },
+  },
+});
+
+export const {
+  getBasketInfo,
+  addProductCount,
+  subProductCount,
+  toggleCheck,
+  addPriceByCount,
+  subPriceByCount,
+  addEachCheckedTotal,
+  subEachCheckedTotal,
+  addAllSum,
+  subAllSum,
+} = basketSlice.actions;
+
+export default basketSlice;
